dc920877f0c94334f343868ff7dc951d446e0f3f
Sean Murphy <sean@iamseanmurphy.com>
2026-04-12 06:46:26 +0000

    3.0.0

---
+++ ultimate-store-kit/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bdthemes/ultimate-store-kit',
+        'pretty_version' => '3.0.0',
+        'version' => '3.0.0.0',
+        'reference' => '5c51de681771305d958915e5d2cbe220be2c0a92',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'bdthemes/ultimate-store-kit' => array(
+            'pretty_version' => '3.0.0',
+            'version' => '3.0.0.0',
+            'reference' => '5c51de681771305d958915e5d2cbe220be2c0a92',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
